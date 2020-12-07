c48754a638e46893c3f4728fe1711d658c8142c7
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2020-12-07 17:48:48 -0600

    Working on the procedures init. Hand constructor

--- LiarsDiceGame/src/LiarsGame/game/gameProcedure.java
+++ LiarsDiceGame/src/LiarsGame/game/gameProcedure.java
@@ -1,5 +1,22 @@
 package LiarsGame.game;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class gameProcedure {
+	
+	// storing first roll
+	private List<Die> diceRoll = new ArrayList<>();
+	
+	// 
+	private List<Integer> bet = new ArrayList<>();
+	
+	// Constructor for initializing Hand
+	public gameProcedure(int size) {
+		for(int count = 0; count < size; count++) {
+			diceRoll.add(new Die());
+		}
+	}
+	
 	
 }
